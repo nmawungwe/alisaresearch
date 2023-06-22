ccae7696fc1928f058b97055e6017195627bc74d
Nyasha <nmawungwe@gmail.com>
2023-06-22 16:03:04 +0200

    Completed Octopus Email integration

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,9 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
 import { sortedBlogPost, allCoreContent } from 'pliny/utils/contentlayer'
-import { NewsletterForm } from 'pliny/ui/NewsletterForm'
+// import { NewsletterForm } from 'pliny/ui/NewsletterForm'
+import { BlogNewsLetterForm } from '@/components/NewsletterForm'
+
 import { allBlogs } from 'contentlayer/generated'
 const MAX_DISPLAY = 5
 export const getStaticProps = async () => {
@@ -20,27 +22,40 @@ export default function Home({ posts }) {
   return (
     <>
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
-      <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Latest
-          </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            {siteMetadata.description}
-          </p>
+      <div>
+        <div className="flex flex-col items-center my-6 xl:flex-row gap-x-12 xl:mb-12">
+          <div className="pt-6">
+            <h1 className="pb-6 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+              Hi, I’m Nyasha
+            </h1>
+            <h2 className="text-lg prose text-gray-600 dark:text-gray-400">
+              {`Welcome to my blog - ${siteMetadata.description}, I enjoy reading and following the technology and finance sectors. In my free time, I enjoy developing `}
+              <Link href="/projects">side projects</Link>
+              {' and '}
+              <Link href="/blog">blogging</Link>
+              {' about them. Have a good read and signup for my future blog posts!'}
+            </h2>
+          </div>
+          <div className="flex items-center justify-center mx-2 my-12 w-96">
+            <BlogNewsLetterForm  title="Stay updated, receive the latest post straight to your mailbox" />
+          </div>
         </div>
+        <h2 className="flex pb-6 text-2xl font-extrabold tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl md:text-5xl">
+          Latest
+        </h2>
+        <hr className="border-gray-200 dark:border-gray-700" />
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
           {!posts.length && 'No posts found.'}
-          {posts.slice(0, MAX_DISPLAY).map((post) => {
-            const { slug, date, title, summary, tags } = post
+          {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
+            const { slug, date, title, summary, tags } = frontMatter
             return (
               <li key={slug} className="py-12">
                 <article>
-                  <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+                  <div className="space-y-2 xl:grid xl:grid-cols-4 xl:space-y-0 xl:items-baseline">
                     <dl>
                       <dt className="sr-only">Published on</dt>
                       <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                        <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
+                        <time dateTime={date}>{formatDate(date)}</time>
                       </dd>
                     </dl>
                     <div className="space-y-5 xl:col-span-3">
@@ -60,7 +75,7 @@ export default function Home({ posts }) {
                             ))}
                           </div>
                         </div>
-                        <div className="prose max-w-none text-gray-500 dark:text-gray-400">
+                        <div className="prose text-gray-500 max-w-none dark:text-gray-400">
                           {summary}
                         </div>
                       </div>
@@ -86,15 +101,10 @@ export default function Home({ posts }) {
           <Link
             href="/blog"
             className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-            aria-label="All posts"
+            aria-label="all posts"
           >
             All Posts &rarr;
           </Link>
-        </div>
-      )}
-      {siteMetadata.newsletter.provider && (
-        <div className="flex items-center justify-center pt-4">
-          <NewsletterForm />
         </div>
       )}
     </>
