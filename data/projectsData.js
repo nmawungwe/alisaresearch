8be10915c5aec8fe4063456e3077f4345b6b0610
Nyasha <nmawungwe@gmail.com>
2024-06-09 23:03:50 +0900

    removed depleb images, made a blog post for LLMOS

--- data/projectsData.js
+++ data/projectsData.js
@@ -5,5 +5,11 @@ const projectsData = [
     imgSrc: '/static/images/FitGPT.png',
     href: 'https://fit-gpt-playground.vercel.app',
   },
+  {
+    title: 'LLMOS',
+    description: `LLMOS is a proof of concept of using an LLM as OS, the application is designed to help you with everyday tasks and boost productivity. Powered by OpenAI API, it functions as an operating system with access to tools like a browser, search engine, calculator, and memory. LLMOS can assist with financial analysis, research, project management, and more. Whether you’re handling complex data or simple tasks, LLMOS adapts to your needs, offering precise and efficient support. It’s like having a helpful assistant always available, anytime, anywhere. Simplify your tasks and enhance your productivity with LLMOS.`,
+    imgSrc: '/static/images/LLMOS.png',
+    href: 'http://llm-os-prd-app-lb-1097205110.us-east-1.elb.amazonaws.com',
+  },
 ]
 export default projectsData
