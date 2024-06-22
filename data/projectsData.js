e8c5db8b270fcd1a460b805bd9edc2a349e68ff4
Nyasha <nmawungwe@gmail.com>
2024-06-22 21:52:35 +0900

    added micrograd project

--- data/projectsData.js
+++ data/projectsData.js
@@ -9,7 +9,14 @@ const projectsData = [
     title: 'LLMOS',
     description: `LLMOS is a proof of concept of using an LLM as OS, the application is designed to help you with everyday tasks and boost productivity. Powered by OpenAI API, it functions as an operating system with access to tools like a browser, search engine, calculator, and memory. LLMOS can assist with financial analysis, research, project management, and more. Whether you’re handling complex data or simple tasks, LLMOS adapts to your needs, offering precise and efficient support. It’s like having a helpful assistant always available, anytime, anywhere. Simplify your tasks and enhance your productivity with LLMOS.`,
     imgSrc: '/static/images/LLMOS.png',
-    href: 'http://llm-os-prd-app-lb-1097205110.us-east-1.elb.amazonaws.com',
+    href: 'https://github.com/nmawungwe/LLMOS',
   },
+  {
+    title: 'Micrograd',
+    description: `Understanding Backpropagation: My Journey with the Chain Rule explores neural network learning through a hands-on approach. This project features a custom Python class for simulating neural network computations, visualization tools for understanding backpropagation, and practical code examples. By breaking down complex concepts and providing GitHub resources, it offers an accessible way to grasp the fundamentals of how neural networks training.`,
+    imgSrc: '/static/Backprop.png',
+    href: 'https://github.com/nmawungwe/micrograd/',
+  }
+
 ]
 export default projectsData
